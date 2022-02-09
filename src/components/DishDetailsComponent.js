65eb3f864d14b1204af15e19ecaa039fa31ab832
vietnguyxn <vietnguyenhht@gmail.com>
2022-02-09 15:20:16 +0700

    Function Components

---
+++ src/components/DishDetailsComponent.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import {Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';
+
+
+
+    
+    function RenderDish({dish}){
+        return(
+        <div className="col-12 col-md-5 m-1">
+            <Card>
+                <CardImg src={dish.image} alt={dish.name}/>
+                <CardBody>
+                    <CardTitle>
+                        {dish.name}
+                    </CardTitle>
+                    <CardText>
+                        {dish.decription}
+                    </CardText>
+                </CardBody>
+            </Card>
+         </div>
+        );
+    }
+    function RenderComments({comments}){
+        if(comments !=null){
+            const commentListItems = comments.map((comment) => {
+                return(
+                    <li key={comment.id}>
+                        <p>{comment.comment}</p>
+                        <p>--{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                    </li>
+                );
+            });
+                return(
+                    <div className="col-12 col-md-5 m-1">
+                        <h4>Comments</h4>
+                        <ul className="list-unstyled">
+                            {commentListItems}
+                        </ul>
+                    </div>
+                );
+        }
+        else {
+            return(
+                <div></div>
+            );
+        }
+    }
+    const DishDetail =(props) =>{
+        if(props.dish != null){
+        return(
+                <div className="container">
+                    <div className="row">
+                        <RenderDish dish={props.dish}/>
+                        <RenderComments comments={props.dish.comments}/>
+                    </div>
+                </div>
+        );
+        }
+        else{
+            return(
+                <div></div>
+            );
+        }
+    }
+
+export default DishDetail;
