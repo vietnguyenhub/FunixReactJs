098dd03ce3324084aa6fc5a9ad155127b01bbc85
vietnguyxn <vietnguyenhht@gmail.com>
2022-02-10 10:12:24 +0700

    React Router

---
+++ src/components/MainComponent.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import Menu from './MenuComponent';
+import { DISHES } from '../shared/dishes';
+import DishDetail from './DishDetailsComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        dishes: DISHES
+    }
+}
+  
+
+  render() {
+
+    const HomePage = () => {
+      return (
+          <Home/>
+      )
+    }
+    return (
+      <div className="App">
+          <Header/>
+            <Switch>
+              <Route path='/home' component={HomePage}/>
+              <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />}/>
+              <Redirect to='/home'/>
+            </Switch>
+          <Footer/>
+      </div>
+    );
+  }
+}
+
+export default Main;
