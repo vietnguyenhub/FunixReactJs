54dc1dd0f48b0c015d50c1ccee36b5a134ea38f6
vietnguyxn <vietnguyenhht@gmail.com>
2022-02-11 10:18:55 +0700

    combine Reducer

---
+++ src/redux/configureStore.js
@@ -0,0 +1,18 @@
+import {createStore, combineReducers} from 'redux';
+import { Dishes } from './dishes';
+import { Leaders } from './leaders';
+import { Comments } from './comments';
+import { Promotions } from './promotions';
+
+export const ConfigureStore = () => {
+    const store = createStore(
+        combineReducers({
+            dishes: Dishes,
+            comments: Comments,
+            leaders: Leaders,
+            promotions: Promotions
+        })
+    );
+
+    return store;
+}
