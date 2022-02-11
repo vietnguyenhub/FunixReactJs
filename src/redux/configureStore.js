24622a756b7f5218d3ffcdede054a92a4a86c254
vietnguyxn <vietnguyenhht@gmail.com>
2022-02-11 11:03:19 +0700

    redux thunk

---
+++ src/redux/configureStore.js
@@ -0,0 +1,21 @@
+import {createStore, combineReducers, applyMiddleware } from 'redux';
+import { Dishes } from './dishes';
+import { Leaders } from './leaders';
+import { Comments } from './comments';
+import { Promotions } from './promotions';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+export const ConfigureStore = () => {
+    const store = createStore(
+        combineReducers({
+            dishes: Dishes,
+            comments: Comments,
+            leaders: Leaders,
+            promotions: Promotions
+        }),
+        applyMiddleware(thunk, logger)
+    );
+
+    return store;
+}
